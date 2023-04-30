cf6a835731224974e7f09ba3207f927dfeb5242d
Jonathan-404 <yt.gamerstop.yt@gmail.com>
2023-04-30 20:42:00 +0300

    Main Functions Setup

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from keys import discord_webhook
+from bs4 import BeautifulSoup
 import requests
 import time
-import bs4
 
 
-print("Hello World!")
+urls = ['']
+keywords = ['']
+
+
+def main():
+    pass
+
+
+def check_price(urls: list):
+    pass
+
+
+def webhook_send(discord_webhook: str, message: str):
+    pass
 

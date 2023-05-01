2a729ed1194fecad138aeece5815ad1da8ee3d0a
Jonathan-404 <yt.gamerstop.yt@gmail.com>
2023-05-01 14:54:01 +0300

    webhook_send fixes

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ from bs4 import BeautifulSoup
 import requests
 import time
 
+# sites algorithms are imported from sites directory
+
+from sites import jd_sports
+print(jd_sports.x)
+
 
 def main():
-    pass
+    check_keywords(urls, keywords)
 
 
 def check_keywords(urls: list, keywords: list):
@@ -25,16 +30,17 @@ def check_keywords(urls: list, keywords: list):
         for product_item in product_items:
             h2 = product_item.find('h2', class_='product-item-meta__title')
 
-            title = h2.text.strip()
+            product_name = h2.text.strip()
 
             for keyword in keywords:
-                if keyword in title:
+                if keyword in product_name:
 
                     url_paramater = h2.get('href')
                     link = f"https://www.jdsports.co.il{url_paramater}"
 
                     if link not in old_links:
                         old_links.append(link)
+
                     break
 
     old_links_file = open("old_links.txt", "w")
@@ -48,10 +54,10 @@ def check_price(url: str):
     soup = BeautifulSoup(content, 'html.parser')
 
     price = soup.find('span', class_='price price--large')
-
     return ''.join(filter(lambda x: x.isdigit() or x == '.', price.text))
 
 
+# webhook_send(discord_webhook, "JD Sports", title, link, check_price(link), "In Stock", [], "")
 def webhook_send(discord_webhook: str, website_name: str, product_name: str, product_link: str, product_price: str, product_status: str, product_sizes: list, product_image: str):
     discord = Discord(url=discord_webhook)
 

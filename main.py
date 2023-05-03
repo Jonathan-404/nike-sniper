044dae503f84381c7795ad40a2b033c6d704e916
Jonathan-404 <yt.gamerstop.yt@gmail.com>
2023-05-03 18:11:20 +0300

    webhook massage changed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from keys import keywords
+import time
 
 # sites algorithms
 from sites.jd_sports import jd_sports
@@ -9,7 +10,11 @@ jd_sports_url = "https://www.jdsports.co.il/collections/men-shoes-sneakers?page=
 
 def main():
     while True:
+        start = time.time()
         jd_sports.new_product_urls(jd_sports_url, keywords)
+        end = time.time()
+        print(f"Time taken: {end - start} seconds")
+        # time.sleep(60)
 
 
 if __name__ == "__main__":
--- sites/jd_sports/scarpe_functions.py
+++ sites/jd_sports/scarpe_functions.py
@@ -3,26 +3,19 @@ import requests
 import json
 
 
-def scrape_product_price(url: str):
-    content = requests.get(url).content
-    soup = BeautifulSoup(content, 'html.parser')
+def scrape_product_price(soup):
     if soup.find('span', class_='price price--large'):
         return ''.join(filter(lambda x: x.isdigit() or x == '.', soup.find('span', class_='price price--large').text))
     elif soup.find('span', class_='price price--highlight price--large'):
         return ''.join(filter(lambda x: x.isdigit() or x == '.', soup.find('span', class_='price price--highlight price--large').text))
 
 
-def scrape_product_image(url: str):
-    content = requests.get(url).content
-    soup = BeautifulSoup(content, 'html.parser')
+def scrape_product_image(soup):
     return soup.find('div', class_='product__media-image-wrapper').find('img').get('src')[2:]
 
 
-def scrape_product_sizes(url: str):
+def scrape_product_sizes(soup):
     sizes = []
-
-    content = requests.get(url).content
-    soup = BeautifulSoup(content, 'html.parser')
 
     for size in json.loads(soup.find('script', type='application/ld+json').text)['offers']:
         if size['availability'] == 'https://schema.org/InStock':

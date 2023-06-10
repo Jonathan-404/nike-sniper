459c3bdd76ca6805fad90c1a8fa076a06448153c
LiamOhayon <liamohayon2468@gmail.com>
2023-06-10 15:08:58 +0300

    nike structure change

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ import time
 
 # sites algorithms
 from src.jd_sports import jd_sports
+
 from src.terminal_x import terminal_x
 #from src.factory_54 import factory_54
 from src.sneakerboxtlv import sneakerboxtlv
 from src.notforsale import notforsale
+
+from src.nike import nike
+
+
 
 # sites urls
 jd_sports_urls = ["https://www.jdsports.co.il/collections/men-shoes-sneakers?page=", "https://www.jdsports.co.il/collections/women-shoes-sneakers?page="]
@@ -15,6 +20,7 @@ terminal_x_url = "https://www.terminalx.com/men/shoes/sneakers-shoes?p="
 factory_54_url = ["https://www.factory54.co.il/men-shoes-sneakers?start="]
 sneakerboxtlv_url = "https://sneakerboxtlv.com/wp-admin/admin-ajax.php"
 notforsale_url = "https://notforsaletlv.com/collections/sneakers?page="
+nike_url = "https://www.nike.com/il/w/mens-shoes-nik1zy7ok"
 
 
 def main():
@@ -36,6 +42,12 @@ def main():
 
     sneakerboxtlv_process = Process(target=sneakerboxtlv.get, args=(sneakerboxtlv_url, keywords))
     processes.append(sneakerboxtlv_process)
+
+    nike_first_process = Process(target=nike.get_first, args=(nike_url, keywords))
+    processes.append(nike_first_process)
+
+    nike_api_process = Process(target=nike.get_api, args=(nike_url, keywords))
+    processes.append(nike_api_process)
 
     # notforsale_process = Process(target=notforsale.new_product_urls, args=(notforsale_url, keywords))
 

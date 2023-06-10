2140ab771bda46470ccec913870fb33e68264556
LiamOhayon <liamohayon2468@gmail.com>
2023-06-10 18:53:15 +0300

    accurate nike anchor

--- src/nike/nike.py
+++ src/nike/nike.py
@@ -87,7 +87,7 @@ def process_api_product_info(product):
 
 
 def get_api(url: str, keywords: list):
-    anchor = 0
+    anchor = 24
     api_url = f"https://api.nike.com/cic/browse/v2?queryid=products&anonymousId=CFD2FD8E39261E9F8455D413E1597666&country=il&endpoint=%2Fproduct_feed%2Frollup_threads%2Fv2%3Ffilter%3Dmarketplace(IL)%26filter%3Dlanguage(en-GB)%26filter%3DemployeePrice(true)%26filter%3DattributeIds(16633190-45e5-4830-a068-232ac7aea82c%2C0f64ecc7-d624-4e91-b171-b83a03dd8550)%26anchor%3D{anchor}%26consumerChannelId%3Dd9a5bc42-4b9c-4976-858a-f159cf99c647%26count%3D24&language=en-GB&localizedRangeStr=%7BlowestPrice%7D%E2%80%94%7BhighestPrice%7D"
     index = 0
     urls = get_urls("nike")

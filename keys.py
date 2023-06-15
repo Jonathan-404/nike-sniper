38be56923ee6918b507fbe0ec77bce3f3a4b672e
LiamOhayon <liamohayon2468@gmail.com>
2023-06-15 14:57:07 +0300

    accurate nike anchor

--- keys.py
+++ keys.py
@@ -2,7 +2,8 @@
 
 
 # discord webhook url (str)
-discord_webhook = "https://discord.com/api/webhooks/1107307340560089088/L_1k5ub4wUr7evwEv0V2z0lglEhcc3q-jJzkYvVdnAn8HXBtqBR0bKskFal1OBJXtMY6"
+
+discord_webhooks = ["https://discord.com/api/webhooks/1107307340560089088/L_1k5ub4wUr7evwEv0V2z0lglEhcc3q-jJzkYvVdnAn8HXBtqBR0bKskFal1OBJXtMY6", "https://discord.com/api/webhooks/1103316064076562553/wUBkVlXHfv8cbNiUgTTLffQebK1-CPhTpe5YfPqBUb1MLjoHDwZauybcEVQBcv4BKA9P"]
 
 # keywords to look for (list)
 keywords = ['Dunk', 'dunk']
--- src/nike/nike.py
+++ src/nike/nike.py
@@ -61,8 +61,7 @@ def get_first(url: str, keywords: list):
                             shoe.discord_message()
                             shoe.update()
                             urls.append(shoe.url)
-                if shoe.url in urls:
-                    check_for_updates(shoe.sizes, get_stored_sizes(shoe.site, shoe.url), shoe)
+
 
 
 
@@ -115,8 +114,7 @@ def get_api(url: str, keywords: list):
                                 shoe.discord_message()
                                 shoe.update()
                                 urls.append(shoe.url)
-                    if shoe.url in urls:
-                        check_for_updates(shoe.sizes, get_stored_sizes(shoe.site, shoe.url), shoe)
+
 
             anchor += ANCHOR_OFFSET
             api_url = f"https://api.nike.com/cic/browse/v2?queryid=products&anonymousId=CFD2FD8E39261E9F8455D413E1597666&country=il&endpoint=%2Fproduct_feed%2Frollup_threads%2Fv2%3Ffilter%3Dmarketplace(IL)%26filter%3Dlanguage(en-GB)%26filter%3DemployeePrice(true)%26filter%3DattributeIds(16633190-45e5-4830-a068-232ac7aea82c%2C0f64ecc7-d624-4e91-b171-b83a03dd8550)%26anchor%3D{anchor}%26consumerChannelId%3Dd9a5bc42-4b9c-4976-858a-f159cf99c647%26count%3D24&language=en-GB&localizedRangeStr=%7BlowestPrice%7D%E2%80%94%7BhighestPrice%7D"
